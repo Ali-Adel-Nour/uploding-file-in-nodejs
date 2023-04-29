daf22067e3b25b06285a08b15c7127d4bc6a3bdb
Ali-Adel-Nour <aliiiadel841@gmail.com>
2023-04-30 02:14:47 +0300

    done

--- app.js
+++ app.js
@@ -4,27 +4,69 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.set('view engine', 'ejs');
-app.use(fileUpload());
+app.set('view engine', 'ejs')
 
-
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: path.join(__dirname, 'tmp'),
+    createParentPath: true,
+    limits: { fileSize: 2 * 1024 * 1024 },
+  })
+)
 
 app.get('/', async (req, res, next) => {
-    res.render('index');
-});
+  res.render('index')
+})
 
 app.post('/single', async (req, res, next) => {
-    try {
-        const file = req.files.mFile;
-        console.log(file);
-        const fileName = new Date().getTime().toString()+ path.extname(file.name)
-        const savePath = path.join(__dirname,'public', 'uploads', fileName);
-        await file.mv(savePath);
-        res.redirect('/');
-    } catch (error) {
-        console.log(error);
-        res.send('Error uploading file');
+  try {
+    const file = req.files.mFile
+    console.log(file)
+    const fileName = new Date().getTime().toString() + path.extname(file.name)
+    const savePath = path.join(__dirname, 'public', 'uploads', fileName)
+    if (file.truncated) {
+      throw new Error('File size is too big...')
     }
-});
+    if (file.mimetype !== 'image/jpeg') {
+      throw new Error('Only jpegs are supported')
+    }
+    await file.mv(savePath)
+    res.redirect('/')
+  } catch (error) {
+    console.log(error)
+    res.send('Error uploading file')
+  }
+})
+
+app.post('/multiple', async (req, res, next) => {
+  try {
+    const files = req.files.mFiles
+
+    // files.forEach(file => {
+    //   const savePath = path.join(__dirname, 'public', 'uploads', file.name)
+    //   await file.mv(savePath)
+    // })
+
+    // let promises = []
+    // files.forEach((file) => {
+    //   const savePath = path.join(__dirname, 'public', 'uploads', file.name)
+    //   promises.push(file.mv(savePath))
+    // })
+
+    const promises = files.map((file) => {
+      const savePath = path.join(__dirname, 'public', 'uploads', file.name)
+      return file.mv(savePath)
+    })
+
+    await Promise.all(promises)
+
+    res.redirect('/')
+  } catch (error) {
+    console.log(error)
+    res.send('Error uploading files...')
+  }
+})
+
 
 app.listen(PORT, () => console.log(`server listening on ${PORT}`));
